86a16ee3fd820af896ea8db7663ead04a6daff5e
Dhruvit Vaghani <dhruvitvaghani123@gmail.com>
2023-03-15 13:34:40 +0100

    Revert "just checking revert"
    This reverts commit 52aab15577cfa048de8e1a73fff24e505f592458.

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const os = require('os');
-const { allowedNodeEnvironmentFlags, features } = require('process');
 
 //here is the how we can calculate the total memory in node.js
 var tm = os.totalmem();
 var fm = os.freemem();
 
 console.log('Total Memory:' + tm);
-
-frekahfewhufuahhae
-
-FileSystemFileHandleeaf
-allowedNodeEnvironmentFlagsafa
-featuresfea
-f testing revert commit
