cddd118ef1e4cbe1eb2efd53d9bf0c697c8aa6e1
Dhruvit Vaghani <dhruvitvaghani123@gmail.com>
2023-03-18 14:53:53 +0100

    handling error

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const fs = require('fs');
+const fs = require("fs");
+const http = require("http");
+// //Blocking, sync
+// const readfile = fs.readFileSync('./text/input.txt','utf-8');
+// console.log(readfile);
 
-const readfile = fs.readFileSync('./text/input.txt','utf-8')
-console.log(readfile)
+// const writefile = `${readfile} Dhruvit.\n time:${Date.now()}`
+// fs.writeFileSync('./text/output.txt',writefile);
 
-const writefile = `${readfile} Dhruvit.\n time:${Date.now()}`
-fs.writeFileSync('./text/output.txt',writefile)
+// //Non-blocking, Async
+// fs.readFile("./text/input.txt", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+// console.log("Reading file");
+
+const server = http.createServer((req, res) => {
+  const pathnName = req.url;
+  if (pathnName === "/" || pathnName === "/overview") {
+    res.end("This is overview");
+  } else if (pathnName === "/product") res.end("This is product");
+  else res.end("Page not found");
+});
+
+server.listen(8000, "localhost", () => {
+  console.log("Server strated on port 8000");
+});
